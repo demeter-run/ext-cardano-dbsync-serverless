13716d415b4adf144e1d03f82ba7a7c5d37970a3
paulobressan <paulo.bressan@outlook.com>
2024-09-23 17:02:21 -0300

    chore: implemented crdgen json output

--- operator/src/crdgen.rs
+++ operator/src/crdgen.rs
@@ -2,6 +2,15 @@ use ext_cardano_dbsync::controller;
 use kube::CustomResourceExt;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && args[1] == "json" {
+        print!(
+            "{}",
+            serde_json::to_string_pretty(&controller::DbSyncPort::crd()).unwrap()
+        );
+        return;
+    }
+
     print!(
         "{}",
         serde_yaml::to_string(&controller::DbSyncPort::crd()).unwrap()
